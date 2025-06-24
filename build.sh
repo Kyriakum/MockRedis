7cd2de59dc5b87d7007ff6224b2b0a6aeed703e4
Kyriakum <kyriakos186@gmail.com>
2025-06-24 15:47:02 +0300

    added ASIO

--- build.sh
+++ build.sh
@@ -2,7 +2,14 @@
 
 set -e
 (
+mkdir -p build
+
 cd "$(dirname "$0")"
-cmake -B build -S . -DCMAKE_TOOLCHAIN_FILE=~/vcpkg/scripts/buildsystems/vcpkg.cmake
+
+vcpkg install --x-install-root=build/vcpkg_installed
+
+cmake -B build \
+    -S . \
+    -DCMAKE_TOOLCHAIN_FILE=~/vcpkg/scripts/buildsystems/vcpkg.cmake
 cmake --build ./build
 )
---
+++ src/server/Server.hpp
@@ -0,0 +1,8 @@
+#ifndef SERVER_HPP
+#define SERVER_HPP
+
+namespace Server {
+
+}
+
+#endif //SERVER_HPP
