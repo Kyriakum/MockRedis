c6097b0f34df54c71fa96647f312c50bb08f2441
Kyriakum <kyriakos186@gmail.com>
2025-06-27 13:26:58 +0300

    added async_read_until in start stub func

--- src/server/ClientSession.cpp
+++ src/server/ClientSession.cpp
@@ -1,6 +1,11 @@
 #include "ClientSession.hpp"
 
-void RedisServer::ClientSession::start() {}
+void RedisServer::ClientSession::start() {
+  auto self = shared_from_this();
+  asio::async_read_until(m_socket, m_buffer, "\r\n", [self](const std::error_code& ec, std::size_t bytes_transferred) {
+     // TODO setup parsing sequence
+  });
+}
 void RedisServer::ClientSession::parse_header() {}
 void RedisServer::ClientSession::parse_body() {}
 void RedisServer::ClientSession::send_response() {}
